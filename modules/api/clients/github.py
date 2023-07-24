4bf337cfd0007ab3d3436e816d8eda0ba54057eb
Andrii Shyts <andrewszyc@ukr.net>
2023-07-24 16:41:54 +0300

    Added GitHub_API individual tasks

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -15,3 +15,19 @@ class GitHub:
         body = r.json()
 
         return body
+
+    # Individual tasks
+
+    def search_emoji(self):
+        r = requests.get("https://api.github.com/emoji")
+        body = r.json()
+
+        return body
+
+    def interact_with_commits(self, owner_name, repo_name):
+        r = requests.get(
+            f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+        )
+        body = r.json()
+
+        return body
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -35,3 +35,48 @@ def test_repo_with_single_char_be_found(github_api):
     repo = github_api.search_repo("s")
 
     assert repo["total_count"] != 0
+
+
+# Individual tasks
+
+
+@pytest.mark.api
+def test_all_emojis_can_be_found(github_api):
+    emoji = github_api.search_emoji()
+    assert "messege" not in emoji
+
+
+@pytest.mark.api
+def test_can_be_found_special_emoji(github_api):
+    emoji = github_api.search_emoji()
+    assert "zombie" in emoji
+
+
+@pytest.mark.api
+def test_cannot_be_found_emoji(github_api):
+    emoji = github_api.search_emoji()
+    assert "andrii" not in emoji
+
+
+@pytest.mark.api
+def test_not_finde_commits(github_api):
+    commit = github_api.interact_with_commits("butenkosergii", "become-qa-auto")
+    assert commit["message"] == "Not Found"
+
+
+@pytest.mark.api
+def test_finde_email_commit(github_api):
+    commit = github_api.interact_with_commits("AndriiSzyc", "SzycQAauto")
+    assert commit[0]["commit"]["committer"]["email"] == "andrewszyc@ukr.net"
+
+
+@pytest.mark.api
+def test_chek_message_commit(github_api):
+    commit = github_api.interact_with_commits("AndriiSzyc", "SzycQAauto")
+    assert commit[0]["commit"]["message"] != "null"
+
+
+@pytest.mark.api
+def test_chek_reponame_commit(github_api):
+    commit = github_api.interact_with_commits("AndriiSzyc", "SzycQAauto")
+    assert "SzycQAauto" and "AndriiSzyc" in commit[0]["commit"]["tree"]["url"]

706f7db4691777eaff6c23fa55a2602f9a49f0a4
Andrii Shyts <andrewszyc@ukr.net>
2023-08-07 14:42:06 +0300

    Added GIT_TIKEN authorization

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -1,16 +1,27 @@
 import requests
+import os
 
 
 class GitHub:
+    HEADERS = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer " + os.environ.get("GIT_TOKEN"),
+        "X-GitHub-Api-Versio": "2022-11-28",
+    }
+
     def get_user(self, username):
-        r = requests.get(f"https://api.github.com/users/{username}")
+        r = requests.get(
+            f"https://api.github.com/users/{username}", headers=self.HEADERS
+        )
         body = r.json()
 
         return body
 
     def search_repo(self, name):
         r = requests.get(
-            "https://api.github.com/search/repositories", params={"q": name}
+            "https://api.github.com/search/repositories",
+            headers=self.HEADERS,
+            params={"q": name},
         )
         body = r.json()
 
@@ -18,15 +29,16 @@ class GitHub:
 
     # Individual tasks
 
-    def search_emoji(self):
-        r = requests.get("https://api.github.com/emoji")
+    def get_emojis(self):
+        r = requests.get("https://api.github.com/emojis", headers=self.HEADERS)
         body = r.json()
 
         return body
 
-    def interact_with_commits(self, owner_name, repo_name):
+    def list_commits(self, owner_name, repo_name):
         r = requests.get(
-            f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+            f"https://api.github.com/repos/{owner_name}/{repo_name}/commits",
+            headers=self.HEADERS,
         )
         body = r.json()
 
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -19,7 +19,7 @@ def test_user_not_exists(github_api):
 def test_repo_can_be_found(github_api):
     repo = github_api.search_repo("become-qa-auto")
 
-    assert repo["total_count"] == 42
+    assert repo["total_count"] == 43
     assert "become-qa-auto" in repo["items"][0]["name"]
 
 
@@ -42,41 +42,47 @@ def test_repo_with_single_char_be_found(github_api):
 
 @pytest.mark.api
 def test_all_emojis_can_be_found(github_api):
-    emoji = github_api.search_emoji()
+    emoji = github_api.get_emojis()
     assert "messege" not in emoji
 
 
 @pytest.mark.api
 def test_can_be_found_special_emoji(github_api):
-    emoji = github_api.search_emoji()
+    emoji = github_api.get_emojis()
     assert "zombie" in emoji
 
 
 @pytest.mark.api
 def test_cannot_be_found_emoji(github_api):
-    emoji = github_api.search_emoji()
+    emoji = github_api.get_emojis()
     assert "andrii" not in emoji
 
 
 @pytest.mark.api
 def test_not_finde_commits(github_api):
-    commit = github_api.interact_with_commits("butenkosergii", "become-qa-auto")
+    commit = github_api.list_commits("butenkosergii", "become-qa-auto")
     assert commit["message"] == "Not Found"
 
 
 @pytest.mark.api
 def test_finde_email_commit(github_api):
-    commit = github_api.interact_with_commits("AndriiSzyc", "SzycQAauto")
+    commit = github_api.list_commits("AndriiSzyc", "SzycQAauto")
     assert commit[0]["commit"]["committer"]["email"] == "andrewszyc@ukr.net"
 
 
 @pytest.mark.api
 def test_chek_message_commit(github_api):
-    commit = github_api.interact_with_commits("AndriiSzyc", "SzycQAauto")
+    commit = github_api.list_commits("AndriiSzyc", "SzycQAauto")
     assert commit[0]["commit"]["message"] != "null"
 
 
 @pytest.mark.api
 def test_chek_reponame_commit(github_api):
-    commit = github_api.interact_with_commits("AndriiSzyc", "SzycQAauto")
+    commit = github_api.list_commits("AndriiSzyc", "SzycQAauto")
     assert "SzycQAauto" and "AndriiSzyc" in commit[0]["commit"]["tree"]["url"]
+
+
+@pytest.mark.api
+def test_parents(github_api):
+    commit = github_api.list_commits("AndriiSzyc", "SzycQAauto")
+    assert commit[-1]["parents"] == []

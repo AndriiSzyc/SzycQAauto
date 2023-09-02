667266d0d717ebb606a54fae9147d623ce475a8f
Andrii Shyts <andrewszyc@ukr.net>
2023-09-02 20:09:05 +0300

    make UI-tests

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -1,43 +1,42 @@
 import pytest
 
-# Modules for communication with browser-driver
+# modules for communication with browser-driver
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-# Module for searching elements by certain attributs
+# module for searching elements by certain attributs
 from selenium.webdriver.common.by import By
 
-# import time
 
 
 @pytest.mark.ui
 def test_check_incorrect_username():
-    # Creating an object to controle browser
+    # creating an object to controle browser
     driver = webdriver.Chrome(
         service=Service(r"/home/andrii/QAauto/SzycQAauto/" + "chromedriver")
     )
 
-    # Open https://github.com/login page
+    # open https://github.com/login page
     driver.get("https://github.com/login")
 
-    # Find field in which we will enter incorrect user-name or e-mail address
+    # find the field in which we will enter the wrong username or postal address
     login_elem = driver.find_element(By.ID, "login_field")
-    # Enter incorrect user-name or e-mail address
-    login_elem.send_keys("andriiszyc@mistakeinemail.com")
 
-    # Find field in which we will enter incorrect Password
+    # enter the wrong username or email address
+    login_elem.send_keys("myemail@mistakeinemail.com")
+
+    # find the field in which we will enter the wrong password
     pass_elem = driver.find_element(By.ID, "password")
-    # Enter incorrect Password
-    pass_elem.send_keys("wrong password")
+    # enter the wrong password
+    pass_elem.send_keys("myemail@mistakeinemail.com")
 
-    # Find "Sign in" button
+    # find button 'Sign in'
     btn_elem = driver.find_element(By.NAME, "commit")
-    # Emulate click left-mouse button
+    # click on the left mouse button
     btn_elem.click()
 
     # Check that name of page is what we expect
     assert driver.title == "Sign in to GitHub · GitHub"
-    # time.sleep(3)
 
-    # Close browser
+    # browser close
     driver.close()
